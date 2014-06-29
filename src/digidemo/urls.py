c9c0302bf785f124a61bd08581e52665fecd2302
Edward Newell <edward.newell@gmail.com>
2014-06-29 16:58:02 -0400

    nothing

--- src/digidemo/urls.py
+++ src/digidemo/urls.py
@@ -7,8 +7,8 @@ from ajax import handle_ajax_json, handle_ajax_html
 admin.autodiscover()
 
 urlpatterns = patterns('',
-        url(r'^mainPage/$',mainPage),
-        url(r'^mainPage/sort=(?P<sort_type>\w+)/$',mainPage,name='mainPage'),
+	url(r'^mainPage/$',mainPage),
+	url(r'^mainPage/sort=(?P<sort_type>\w+)/$',mainPage,name='mainPage'),
 	url(r'^$', test, name='test'),
 	url(r'^proposals/(?P<proposal_name>\w+)/$', proposal, name='proposal'),
 	url(r'^send_letter/$', send_letter, name='send_letter'),
