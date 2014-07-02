a8496d5a312a6bb1739ae8398aeb953130c9f5ad
Edward Newell <edward.newell@gmail.com>
2014-07-02 00:29:08 -0400

    add markdown tag to apptags

--- src/digidemo/templatetags/apptags.py
+++ src/digidemo/templatetags/apptags.py
@@ -1,6 +1,9 @@
 from django import template
 from datetime import date, timedelta
+from django.utils.html import conditional_escape
+from django.utils.safestring import mark_safe
 from digidemo.models import *
+from digidemo import markdown as md
 
 register = template.Library()
 
@@ -10,8 +13,20 @@ def getAuthor(users, proposal):
     print users.get(id=proposal.author_id).user.first_name
     return users.get(id=proposal.author_id).avatar_name
 
+
 @register.filter(name='getSummary')
 def getSummary(proposal):
     returnText = proposal[0:200];
     returnText+= '......';
     return returnText
+
+
+@register.filter(needs_autoescape=True)
+def markdown(text, autoescape=None):
+	if autoescape:
+		esc = conditional_escape
+	else:
+		esc = lambda x: x
+	
+	html = md.markdown(esc(text))
+	return mark_safe(html)
